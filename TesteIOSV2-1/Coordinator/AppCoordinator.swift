f7c0e987abd10baace0df1ddb14e8a9f8a1d3d45
paulodanilolima <37875142+paulodanilolima@users.noreply.github.com>
2022-06-14 19:47:49 -0400

    quarto commit

---
+++ TesteIOSV2-1/Coordinator/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  TesteIOSV2-1
+//
+//  Created by Paulo Danilo Conceição Lima on 07/06/22.
+//
+
+import Foundation
---
+++ TesteIOSV2-1/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  TesteIOSV2-1
+//
+//  Created by Paulo Danilo Conceição Lima on 07/06/22.
+//
+
+import Foundation
